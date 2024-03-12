917d7919a36c937cc07fab5bc63967a95b89e948
lokesh0802 <lokesh08519137@gmail.com>
2024-03-12 20:21:16 +0530

    js added to lognandsinup

---
+++ dist/js/loginuser.js
@@ -0,0 +1,3 @@
+document.getElementById("singup").addEventListener("click", function() {
+    window.location.href = "singupuser.html"; // Replace "newpage.html" with the path to your new HTML page
+  });
