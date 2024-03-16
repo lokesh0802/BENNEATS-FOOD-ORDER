65bb26f01c6382bbc445b852ded86ddae8b6be7d
lokesh0802 <lokesh08519137@gmail.com>
2024-03-16 16:42:55 +0530

    cart

---
+++ dist/js/menukaathi.js
@@ -0,0 +1,3 @@
+document.getElementById("cartbutton").addEventListener("click", function() {
+    window.location.href = "cart.html"; // Replace "newpage.html" with the path to your new HTML page
+  });
