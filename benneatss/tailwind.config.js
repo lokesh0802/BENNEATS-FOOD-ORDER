78d14450f091224f8f8aaf7447f497121aa2efb2
lokesh0802 <lokesh08519137@gmail.com>
2024-04-07 11:33:23 +0530

    mern

---
+++ benneatss/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors: {
+      // Configure your color palette here
+      'whitebganki': '#FFF5EC',
+      'hovercolor':'#FE765E',
+      'blackcolor':'#000000',
+      'white': '#FFFFFF',
+      'blue':'#201B47',
+      'shadow color balck': ' rgba(0, 0, 0, 0.16)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
