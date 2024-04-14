f8cd81491f216dfef40937279f2a3d2d2d0d2032
lokesh0802 <lokesh08519137@gmail.com>
2024-04-14 11:50:33 +0530

    frontend

--- New/src/main.jsx
+++
@@ -1,9 +0,0 @@
-
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './router/Router.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
